5ac6b3f11b544920f88ea814e1819bf5792572d3
irinagaina84-jpg <irina.gaina.84@gmail.com>
2026-08-30 12:15:09 +0500

    Restart MAX container for yearly Mail.ru index

--- worker-v71.js
+++ worker-v71.js
@@ -4,8 +4,8 @@ import botWorker from "./worker.js";
 
 const CONTAINER_INSTANCE = "production";
 const CURRENT_CHAT_ID = "-77828005225953";
-const RUNTIME_RESET_VERSION = "worker-v81-mail-index-alias-runtime-r2";
-const WORKER_VERSION = "worker-v81-mail-index-alias-routing";
+const RUNTIME_RESET_VERSION = "worker-v82-mailru-year-index-runtime-r1";
+const WORKER_VERSION = "worker-v82-mailru-year-index-routing";
 
 export class MaxBotContainer extends ExportMaxBotContainer {
   sleepAfter = "30m";
